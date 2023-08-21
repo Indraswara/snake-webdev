7b2522245257f1b7b2e1e7f2a0e810ade1ad43b4
InTheRain <indrswara@gmail.com>
2023-08-21 10:26:49 +0700

    feat: add body when eats food

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var snakeY = blockSize * 5;
 var velocityX = 0; 
 var velocityY = 0;
 
+var snakeBody = [];
 //food 
 var foodX;
 var foodY;
@@ -36,14 +37,16 @@ function update() {
     context.fillRect(foodX, foodY, blockSize, blockSize);
     
     if(snakeX == foodX && snakeY == foodY){
+        snakeBody.push([foodX, foodY]);
         placeFood();
     }
     context.fillStyle = "lime";
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
-
-
+    for(let i = 0; i<snakeBody.length; i++){
+    context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
 }
 
 function placeFood(){
